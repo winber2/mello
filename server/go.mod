835572882437c8b8948de936f4a5599f08568d1a
Winber Xu <winber1@gmail.com>
2018-11-16 02:48:02 -0800

    Update npm packges, add config file

--- server/go.mod
+++ server/go.mod
@@ -1,6 +1,3 @@
 module mello/server
 
-require (
-	github.com/gorilla/mux v1.6.2
-	github.com/inconshreveable/go-update v0.0.0-20160112193335-8152e7eb6ccf
-)
+require github.com/gorilla/mux v1.6.2
--- server/server.go
+++ server/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 
 	"github.com/gorilla/mux"
@@ -11,12 +14,35 @@ import (
 const staticDirectory string = "/static/"
 const clientDirectory string = "/client/public/"
 
+// Configuration determines the environment variables of the server
+type Configuration struct {
+	APIKey string `json:"apiKey"`
+}
+
+func loadConfiguration() {
+	jsonFile, err := os.Open("config.json")
+
+	if err != nil {
+		fmt.Println("No configuration found. Make sure you have a config.json file inside the server folder.")
+		log.Fatal(err)
+	}
+
+	var config Configuration
+	var jsonParser = json.NewDecoder(jsonFile)
+	jsonParser.Decode(&config)
+
+	defer jsonFile.Close()
+	os.Setenv("API_KEY", config.APIKey)
+}
+
 func main() {
 	dir, err := filepath.Abs(filepath.Dir("../"))
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	loadConfiguration()
 
 	r := mux.NewRouter()
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir+staticDirectory))))
@@ -27,6 +53,7 @@ func main() {
 
 	http.Handle("/", r)
 	if err := http.ListenAndServe(":8000", nil); err != nil {
-		panic(err)
+		fmt.Println("Server crashed and burned")
+		log.Fatal(err)
 	}
 }
